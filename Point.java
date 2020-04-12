240c098c3e5bff788084668876e1a1d0c8e9f80f
trpovskigeorgi <63557696+trpovskigeorgi@users.noreply.github.com>
2020-04-12 23:15:31 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -1,20 +1,68 @@
 class Point {
-	String id
-	double x,y;
-	String boja;
-	//TODO add new variable
+    String id;
+    double x,y;
 
-	//TODO constructor
+    //TODO add new variable
+    String color;
+    //TODO constructor
 
-	//TODO setters and getters
+    public Point(String id, double x, double y, String color) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
 
-	public void move (char xDirection, char yDirection) {
-		//TODO
-	}
+    //TODO setters and getters
 
-	public void draw () {
-		//TODO
-	}
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void move (char xDirection, char yDirection) {
+        //TODO
+        if(xDirection=='L')
+            x--;
+        else if(xDirection=='R')
+            x++;
+        if(yDirection=='D')
+            y--;
+        else if(yDirection=='U')
+            y++;
+    }
+
+    public void draw () {
+        //TODO
+        System.out.println("("+x+", "+y+")");
+    }
 
 
 
